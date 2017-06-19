047449eba1c5afaf970d6b3a250d077c67e77494
pehyuhan <pehyuhan@gmail.com>
2017-06-19 17:52:47 +0800

    Add Restaurant

--- FoodTrail/AddTableViewController.swift
+++ FoodTrail/AddTableViewController.swift
@@ -10,7 +10,16 @@ import UIKit
 
 class AddTableViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    @IBOutlet weak var nameTextField:UITextField!
+    @IBOutlet weak var typeTextField:UITextField!
+    @IBOutlet weak var locationTextField:UITextField!
     @IBOutlet weak var imageView:UIImageView!
+    @IBOutlet weak var yesButton:UIButton!
+    @IBOutlet weak var noButton:UIButton!
+    
+    var isVisited = true
+    
+    var item:Item!
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
@@ -46,6 +55,54 @@ class AddTableViewController: UITableViewController, UIImagePickerControllerDele
             }
         }
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    @IBAction func save() {
+        
+        // Form validation
+        var errorField = ""
+        
+        if nameTextField.text == "" {
+            errorField = "name"
+        } else if locationTextField.text == "" {
+            errorField = "location"
+        } else if typeTextField.text == "" {
+            errorField = "type"
+        }
+        
+        if errorField != "" {
+            
+            let alertController = UIAlertController(title: "Oops", message: "We can't proceed as you forget to fill in the restaurant " + errorField + ". All fields are mandatory.", preferredStyle: .alert)
+            let doneAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(doneAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+            
+            return
+        }
+        
+        // If all fields are correctly filled in, extract the field value
+        print("Name: " + nameTextField.text!)
+        print("Type: " + typeTextField.text!)
+        print("Location: " + locationTextField.text!)
+        print("Have you been here: " + (isVisited ? "yes" : "no"))
+        
+        // Execute the unwind segue and go back to the home screen
+        performSegue(withIdentifier: "unwindToHomeScreen", sender: self)
+    }
+    
+    @IBAction func updateIsVisited(sender: AnyObject) {
+        // Yes button clicked
+        let buttonClicked = sender as! UIButton
+        if buttonClicked == yesButton {
+            isVisited = true
+            yesButton.backgroundColor = UIColor(red: 235.0/255.0, green: 73.0/255.0, blue: 27.0/255.0, alpha: 1.0)
+            noButton.backgroundColor = UIColor.gray
+        } else if buttonClicked == noButton {
+            isVisited = false
+            yesButton.backgroundColor = UIColor.gray
+            noButton.backgroundColor = UIColor(red: 235.0/255.0, green: 73.0/255.0, blue: 27.0/255.0, alpha: 1.0)
+        }
     }
     
     /*
